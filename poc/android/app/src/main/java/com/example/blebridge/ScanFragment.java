d64c8c9a6afc44b97f493a8b15390e159e6402e8
Kijong Uhm <kjeom@humaxdigital.com>
2021-03-22 19:16:24 +0900

    add ScanFragment and switching between Fragments

--- poc/android/app/src/main/java/com/example/blebridge/ScanFragment.java
+++ poc/android/app/src/main/java/com/example/blebridge/ScanFragment.java
@@ -11,18 +11,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.blebridge.model.Device;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class DeviceListFragment extends Fragment {
-    private static final String TAG = DeviceListFragment.class.getSimpleName();
+public class ScanFragment extends Fragment {
+    private static final String TAG = ScanFragment.class.getSimpleName();
     protected RecyclerView mRecyclerView;
-    protected DeviceListAdapter mAdapter;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    protected ScanListAdapter mAdapter;
 
     @Override
     public View onCreateView(
@@ -30,26 +24,21 @@ public class DeviceListFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.device_list_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.scan_list_fragment, container, false);
         rootView.setTag(TAG);
 
         // BEGIN_INCLUDE(initializeRecyclerView)
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.device_list_recycler_view);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.scan_list_recycler_view);
 
         // LinearLayoutManager is used here, this will layout the elements in a similar fashion
         // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
         // elements are laid out.
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        mAdapter = new DeviceListAdapter();
+        mAdapter = new ScanListAdapter();
         // Set CustomAdapter as the adapter for RecyclerView.
         mRecyclerView.setAdapter(mAdapter);
         // END_INCLUDE(initializeRecyclerView)
-
-        for (int i = 0; i < 10; i++) {
-            Device device = new Device();
-            mAdapter.addDevice(device);
-        }
 
         return rootView;
     }
@@ -57,16 +46,16 @@ public class DeviceListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton)getActivity().findViewById(R.id.fab);
         if (fab != null) {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    NavHostFragment.findNavController(DeviceListFragment.this)
-                            .navigate(R.id.action_DeviceListFragment_to_ScanFragment);
+                    NavHostFragment.findNavController(ScanFragment.this)
+                            .navigate(R.id.action_ScanFragment_to_DeviceListFragment);
                 }
             });
-            fab.setImageResource(android.R.drawable.ic_input_add);
+            fab.setImageResource(android.R.drawable.ic_media_rew);
         }
     }
 }
---
+++ poc/android/app/src/main/java/com/example/blebridge/ScanListAdapter.java
@@ -0,0 +1,125 @@
+package com.example.blebridge;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.blebridge.model.Device;
+
+import java.util.ArrayList;
+
+/**
+ * Provide views to RecyclerView with data from mDataSet.
+ */
+public class ScanListAdapter extends RecyclerView.Adapter<ScanListAdapter.ViewHolder> {
+    private static final String TAG = ScanListAdapter.class.getSimpleName();
+
+    private ArrayList<Device> devices;
+
+    // BEGIN_INCLUDE(recyclerViewSampleViewHolder)
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView viewDeviceMacAddress;
+        private final TextView viewDeviceName;
+        private final TextView viewDeviceModelName;
+        private final TextView viewDeviceSignalStrength;
+        private final ImageView viewDeviceImage;
+
+        public ViewHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    Log.d(TAG, "Element " + position + " clicked.");
+                }
+            });
+            viewDeviceMacAddress = (TextView) v.findViewById(R.id.deviceMacAddress);
+            viewDeviceName = (TextView) v.findViewById(R.id.deviceName);
+            viewDeviceModelName = (TextView) v.findViewById(R.id.deviceModelName);
+            viewDeviceSignalStrength = (TextView) v.findViewById(R.id.deviceSignalStrength);
+            viewDeviceImage = (ImageView) v.findViewById(R.id.deviceImage);
+        }
+
+        public TextView getViewDeviceName() {
+            return viewDeviceName;
+        }
+
+        public TextView getViewDeviceModelName() {
+            return viewDeviceModelName;
+        }
+
+        public TextView getViewDeviceMacAddress() {
+            return viewDeviceMacAddress;
+        }
+
+        public TextView getViewDeviceSignalStrength() {
+            return viewDeviceSignalStrength;
+        }
+
+        public ImageView getViewDeviceImage() {
+            return viewDeviceImage;
+        }
+    }
+    // END_INCLUDE(recyclerViewSampleViewHolder)
+
+    /**
+     * Initialize the dataset of the Adapter.
+     */
+    public ScanListAdapter() {
+        devices = new ArrayList<>();
+    }
+
+    // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.device_item, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+    // END_INCLUDE(recyclerViewOnCreateViewHolder)
+
+    // BEGIN_INCLUDE(recyclerViewOnBindViewHolder)
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Log.d(TAG, "Element " + position + " set.");
+
+        // Get element from your dataset at this position and replace the contents of the view
+        // with that element
+        Device device = devices.get(position);
+        viewHolder.getViewDeviceName().setText(device.getName());
+        viewHolder.getViewDeviceModelName().setText(device.getModelName());
+        viewHolder.getViewDeviceMacAddress().setText(device.getMacAddress());
+        viewHolder.getViewDeviceSignalStrength().setText(Integer.toString(device.getSignalStrength()));
+        viewHolder.getViewDeviceImage().setImageResource(R.drawable.dev1_sensor_only);
+    }
+    // END_INCLUDE(recyclerViewOnBindViewHolder)
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return devices.size();
+    }
+
+    public void addDevice(Device device) {
+        devices.add(device);
+        notifyItemInserted(devices.size() - 1);
+    }
+
+    public void removeDevice(int position) {
+        devices.remove(position);
+        notifyItemRemoved(position);
+    }
+}
